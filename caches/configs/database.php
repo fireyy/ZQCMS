a4f9961a7a0cd078b9e9a7cb01f6f66fdcbb3a01
wolftankk <wolftankk@gmail.com>
2012-12-17 14:36:56 +0800

    add database config

---
+++ caches/configs/database.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+    'default' => array(
+	'hostname' => "localhost",
+	'database' => "zqcms",
+	'username' => "root",
+	'password' => "",
+	'tablepre' => "zq_",
+	'charset' => "utf8",
+	'type' => 'mysql',
+	'debug' => false,
+	'pconnect' => 0,
+	'autoconnect' => 0
+    )
+)
+?>
