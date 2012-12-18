8643213947cb7643fd0aca0e3613254a424f75ff
wolftankk <wolftankk@gmail.com>
2012-12-18 13:54:51 +0800

    add funcs

--- zqcms/libs/functions/globals.php
+++ zqcms/libs/functions/globals.php
@@ -1,4 +1,296 @@
 <?php
+/**
+ * 返回经addslashes处理过的字符串或数组
+ * @param $string 需要处理的字符串或数组
+ * @return mixed
+ */
+function new_addslashes($string){
+    if(!is_array($string)) return addslashes($string);
+    foreach($string as $key => $val) $string[$key] = new_addslashes($val);
+    return $string;
+}
+
+/**
+ * 返回经stripslashes处理过的字符串或数组
+ * @param $string 需要处理的字符串或数组
+ * @return mixed
+ */
+function new_stripslashes($string) {
+    if(!is_array($string)) return stripslashes($string);
+    foreach($string as $key => $val) $string[$key] = new_stripslashes($val);
+    return $string;
+}
+
+/**
+ * 返回经htmlspecialchars处理过的字符串或数组
+ * @param $obj 需要处理的字符串或数组
+ * @return mixed
+ */
+function new_html_special_chars($string) {
+    if(!is_array($string)) return htmlspecialchars($string);
+    foreach($string as $key => $val) $string[$key] = new_html_special_chars($val);
+    return $string;
+}
+/**
+ * 安全过滤函数
+ *
+ * @param $string
+ * @return string
+ */
+function safe_replace($string) {
+    $string = str_replace('%20','',$string);
+    $string = str_replace('%27','',$string);
+    $string = str_replace('%2527','',$string);
+    $string = str_replace('*','',$string);
+    $string = str_replace('"','&quot;',$string);
+    $string = str_replace("'",'',$string);
+    $string = str_replace('"','',$string);
+    $string = str_replace(';','',$string);
+    $string = str_replace('<','&lt;',$string);
+    $string = str_replace('>','&gt;',$string);
+    $string = str_replace("{",'',$string);
+    $string = str_replace('}','',$string);
+    $string = str_replace('\\','',$string);
+    return $string;
+}
+
+
+
+/**
+ * 过滤ASCII码从0-28的控制字符
+ * @return String
+ */
+function trim_unsafe_control_chars($str) {
+    $rule = '/[' . chr ( 1 ) . '-' . chr ( 8 ) . chr ( 11 ) . '-' . chr ( 12 ) . chr ( 14 ) . '-' . chr ( 31 ) . ']*/';
+    return str_replace ( chr ( 0 ), '', preg_replace ( $rule, '', $str ) );
+}
+
+/**
+ * 格式化文本域内容
+ *
+ * @param $string 文本域内容
+ * @return string
+ */
+function trim_textarea($string) {
+    $string = nl2br ( str_replace ( ' ', '&nbsp;', $string ) );
+    return $string;
+}
+
+/**
+ * 将文本格式成适合js输出的字符串
+ * @param string $string 需要处理的字符串
+ * @param intval $isjs 是否执行字符串格式化，默认为执行
+ * @return string 处理后的字符串
+ */
+function format_js($string, $isjs = 1) {
+    $string = addslashes(str_replace(array("\r", "\n", "\t"), array('', '', ''), $string));
+    return $isjs ? 'document.write("'.$string.'");' : $string;
+}
+
+/**
+ * 获取当前页面完整URL地址
+ */
+function get_url() {
+    $sys_protocal = isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443' ? 'https://' : 'http://';
+    $php_self = $_SERVER['PHP_SELF'] ? safe_replace($_SERVER['PHP_SELF']) : safe_replace($_SERVER['SCRIPT_NAME']);
+    $path_info = isset($_SERVER['PATH_INFO']) ? safe_replace($_SERVER['PATH_INFO']) : '';
+    $relate_url = isset($_SERVER['REQUEST_URI']) ? safe_replace($_SERVER['REQUEST_URI']) : $php_self.(isset($_SERVER['QUERY_STRING']) ? '?'.safe_replace($_SERVER['QUERY_STRING']) : $path_info);
+    return $sys_protocal.(isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '').$relate_url;
+}
+/**
+ * 字符截取 支持UTF8/GBK
+ * @param $string
+ * @param $length
+ * @param $dot
+ */
+function str_cut($string, $length, $dot = '...') {
+    $strlen = strlen($string);
+    if($strlen <= $length) return $string;
+    $string = str_replace(array(' ','&nbsp;', '&amp;', '&quot;', '&#039;', '&ldquo;', '&rdquo;', '&mdash;', '&lt;', '&gt;', '&middot;', '&hellip;'), array('∵',' ', '&', '"', "'", '“', '”', '—', '<', '>', '·', '…'), $string);
+    $strcut = '';
+    if(strtolower(CHARSET) == 'utf-8') {
+	$length = intval($length-strlen($dot)-$length/3);
+	$n = $tn = $noc = 0;
+	while($n < strlen($string)) {
+	    $t = ord($string[$n]);
+	    if($t == 9 || $t == 10 || (32 <= $t && $t <= 126)) {
+		$tn = 1; $n++; $noc++;
+	    } elseif(194 <= $t && $t <= 223) {
+		$tn = 2; $n += 2; $noc += 2;
+	    } elseif(224 <= $t && $t <= 239) {
+		$tn = 3; $n += 3; $noc += 2;
+	    } elseif(240 <= $t && $t <= 247) {
+		$tn = 4; $n += 4; $noc += 2;
+	    } elseif(248 <= $t && $t <= 251) {
+		$tn = 5; $n += 5; $noc += 2;
+	    } elseif($t == 252 || $t == 253) {
+		$tn = 6; $n += 6; $noc += 2;
+	    } else {
+		$n++;
+	    }
+	    if($noc >= $length) {
+		break;
+	    }
+	}
+	if($noc > $length) {
+	    $n -= $tn;
+	}
+	$strcut = substr($string, 0, $n);
+	$strcut = str_replace(array('∵', '&', '"', "'", '“', '”', '—', '<', '>', '·', '…'), array(' ', '&amp;', '&quot;', '&#039;', '&ldquo;', '&rdquo;', '&mdash;', '&lt;', '&gt;', '&middot;', '&hellip;'), $strcut);
+    } else {
+	$dotlen = strlen($dot);
+	$maxi = $length - $dotlen - 1;
+	$current_str = '';
+	$search_arr = array('&',' ', '"', "'", '“', '”', '—', '<', '>', '·', '…','∵');
+	$replace_arr = array('&amp;','&nbsp;', '&quot;', '&#039;', '&ldquo;', '&rdquo;', '&mdash;', '&lt;', '&gt;', '&middot;', '&hellip;',' ');
+	$search_flip = array_flip($search_arr);
+	for ($i = 0; $i < $maxi; $i++) {
+	    $current_str = ord($string[$i]) > 127 ? $string[$i].$string[++$i] : $string[$i];
+	    if (in_array($current_str, $search_arr)) {
+		$key = $search_flip[$current_str];
+		$current_str = str_replace($search_arr[$key], $replace_arr[$key], $current_str);
+	    }
+	    $strcut .= $current_str;
+	}
+    }
+    return $strcut.$dot;
+}
+
+/**
+ * 获取请求ip
+ *
+ * @return ip地址
+ */
+function ip() {
+    if(getenv('HTTP_CLIENT_IP') && strcasecmp(getenv('HTTP_CLIENT_IP'), 'unknown')) {
+	$ip = getenv('HTTP_CLIENT_IP');
+    } elseif(getenv('HTTP_X_FORWARDED_FOR') && strcasecmp(getenv('HTTP_X_FORWARDED_FOR'), 'unknown')) {
+	$ip = getenv('HTTP_X_FORWARDED_FOR');
+    } elseif(getenv('REMOTE_ADDR') && strcasecmp(getenv('REMOTE_ADDR'), 'unknown')) {
+	$ip = getenv('REMOTE_ADDR');
+    } elseif(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], 'unknown')) {
+	$ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return preg_match ( '/[\d\.]{7,15}/', $ip, $matches ) ? $matches [0] : '';
+}
+
+/**
+ * 产生随机字符串
+ *
+ * @param    int        $length  输出长度
+ * @param    string     $chars   可选的
+ * @return   string     字符串
+ */
+function random($length, $chars = '0123456789') {
+    $hash = '';
+    $max = strlen($chars) - 1;
+    for($i = 0; $i < $length; $i++) {
+	$hash .= $chars[mt_rand(0, $max)];
+    }
+    return $hash;
+}
+
+/**
+ * 将字符串转换为数组
+ *
+ * @param	string	$data	字符串
+ * @return	array	返回数组格式，如果，data为空，则返回空数组
+ */
+function string2array($data) {
+    if($data == '') return array();
+    @eval("\$array = $data;");
+    return $array;
+}
+/**
+ * 将数组转换为字符串
+ *
+ * @param	array	$data		数组
+ * @param	bool	$isformdata	如果为0，则不使用new_stripslashes处理，可选参数，默认为1
+ * @return	string	返回字符串，如果，data为空，则返回空
+ */
+function array2string($data, $isformdata = 1) {
+    if($data == '') return '';
+    if($isformdata) $data = new_stripslashes($data);
+    return addslashes(var_export($data, TRUE));
+}
+
+/**
+ * 转换字节数为其他单位
+ *
+ *
+ * @param	string	$filesize	字节大小
+ * @return	string	返回大小
+ */
+function sizecount($filesize) {
+    if ($filesize >= 1073741824) {
+	$filesize = round($filesize / 1073741824 * 100) / 100 .' GB';
+    } elseif ($filesize >= 1048576) {
+	$filesize = round($filesize / 1048576 * 100) / 100 .' MB';
+    } elseif($filesize >= 1024) {
+	$filesize = round($filesize / 1024 * 100) / 100 . ' KB';
+    } else {
+	$filesize = $filesize.' Bytes';
+    }
+    return $filesize;
+}
+
+/**
+ * 生成sql语句，如果传入$in_cloumn 生成格式为 IN('a', 'b', 'c')
+ * @param $data 条件数组或者字符串
+ * @param $front 连接符
+ * @param $in_column 字段名称
+ * @return string
+ */
+function to_sqls($data, $front = ' AND ', $in_column = false) {
+    if($in_column && is_array($data)) {
+	$ids = '\''.implode('\',\'', $data).'\'';
+	$sql = "$in_column IN ($ids)";
+	return $sql;
+    } else {
+	if ($front == '') {
+	    $front = ' AND ';
+	}
+	if(is_array($data) && count($data) > 0) {
+	    $sql = '';
+	    foreach ($data as $key => $val) {
+		$sql .= $sql ? " $front `$key` = '$val' " : " `$key` = '$val' ";
+	    }
+	    return $sql;
+	} else {
+	    return $data;
+	}
+    }
+}
+
+/**
+ * 生成随机字符串
+ * @param string $lenth 长度
+ * @return string 字符串
+ */
+function create_randomstr($lenth = 4) {
+    return random($lenth, '123456789abcdefghijklmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ');
+}
+
+/**
+ * Function dataformat
+ * 时间转换
+ * @param $n INT时间
+ */
+function dataformat($n) {
+    $hours = floor($n/3600);
+    $minite	= floor($n%3600/60);
+    $secend = floor($n%3600%60);
+    $minite = $minite < 10 ? "0".$minite : $minite;
+    $secend = $secend < 10 ? "0".$secend : $secend;
+    if($n >= 3600){
+	return $hours.":".$minite.":".$secend;
+    }else{
+	return $minite.":".$secend;
+    }
+
+}
+
+
 function preprocessFilter($filter){
     if (gettype($filter) == "array"){
 	return $filter;
@@ -24,7 +316,7 @@ function template($module, $template, $style = '', $output=true) {
     $TEMPLATE_CACHE_PATH = CACHE_PATH . "cache_template" . DIRECTORY_SEPARATOR;
     $TEMPLATE_PATH = ZQCMS_PATH . "templates" . DIRECTORY_SEPARATOR;
     $USER_TEMPLATE_PATH = ZQ_PATH."templates".DIRECTORY_SEPARATOR;
-    
+
     $module = str_replace("/", DIRECTORY_SEPARATOR, $module);
     if (!empty($style) && preg_match('/[a-z0-9\-_]+/is', $style)) {
     } elseif (empty($style) && zq_core::load_config('system', "style")) {
@@ -42,7 +334,7 @@ function template($module, $template, $style = '', $output=true) {
     $smarty->setCompileDir($TEMPLATE_CACHE_PATH."compiles".DIRECTORY_SEPARATOR);
     $smarty->setConfigDir($TEMPLATE_CACHE_PATH."configs".DIRECTORY_SEPARATOR);
     $smarty->setCacheDir($TEMPLATE_CACHE_PATH."caches".DIRECTORY_SEPARATOR);
-    
+
     //先去尝试读取用户定义的模板
     if (file_exists($USER_TEMPLATE_PATH.$style.DIRECTORY_SEPARATOR.$module.DIRECTORY_SEPARATOR.$template.".html")) {
 	$smarty->setTemplateDir($USER_TEMPLATE_PATH.$style);
@@ -80,7 +372,7 @@ function template($module, $template, $style = '', $output=true) {
  */
 function register_template_data($key, $value, $update = true, $get_values=false) {
     static $registered_template_data = array();
-    
+
     if ($get_values) {
 	return $registered_template_data;
     }
