f297afd3be1a0c179b295f6e7fffc77f2e745c75
wolftankk <wolftankk@gmail.com>
2012-12-20 17:25:07 +0800

    update game model

--- zqcms/models/game_model.php
+++ zqcms/models/game_model.php
@@ -38,7 +38,40 @@ class game_model extends model {
 	if (is_array($info) && !empty($info)) {
 	    return $this->updateGame($data);
 	}
+
+	$insert_data = array(
+	    'guid' => $data->guid,
+	    'flag' => getFlags($data->isTop),
+	    'game_name' => trim($data->gameName),
+	    'shorttitle' => trim($data->gameName),
+	    'color' => $data->fontColor,
+	    'description'=> $data->gameDescription,
+	    'source' => $data->copyFrom,
+	    'click' => rand(0, 500),
+	    'rank' => time(),
+	    'pubdate' => time(),
+	    'senddate' => time(),
+	    'lastpost' => time(),
+	    'game_tag' => $data->gameTag,
+	    'game_thumb'=>$data->gameThumb,
+	    'game_effect' => $data->gameEffect,
+	    'game_theme' => $data->gameTheme,
+	    'game_status' => trim($data->gameStatus),
+	    'test_status' => trim($data->testStatus),
+	    'offical_url' => trim($data->officalUrl),
+	    'oper_short_name' => trim($data->operShortName),
+	    'dev_short_Name' => trim($data->devShortName),
+	    'pub_short_name' => trim($data->pubShortName),
+	    'game_avatar' => trim($data->gameAvatar),
+	    'dev_id' => $data->devId,
+	    'oper_id' => $data->operId,
+	    'pub_id' => $data->pubId,
+	    'pinyin' => $data->pinyin,
+	    "game_id" => $data->id
+	);
 	
+	$aid = $this->insert($insert_data, true);
+	//update tag
 	//现在所有的数据都会在这里插入
 	// game_tag  category
 	// effect
@@ -50,14 +83,21 @@ class game_model extends model {
     /**
      * 删除一个游戏
      */
-    public function deleteGame() {
+    public function deleteGame($guid) {
+	$info = $this->get_one(array('guid' => $guid));
+	if (is_array($info) && !empty($info)) {
+	    $id = $info["id"];
 
+	    $this->delete(array('id'=>$id));
+
+	    //delete tag
+	}
     }
 
     /**
-     * 删除一个游戏
+     * 更新游戏
      */
-    public function updateGame() {
+    public function updateGame($data) {
 
     }
 }
