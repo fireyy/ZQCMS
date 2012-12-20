48a155bbfd0dad252482fe442f0eb129cab8fda2
wolftankk <wolftankk@gmail.com>
2012-12-20 14:27:33 +0800

    add block typelist update kaifu count update head.html

---
+++ zqcms/libs/plugins/block.zq_typelist.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * ZQCMS PLUGIN
+ *
+ * ZQCMS 内容模型数据输出
+ */
+
+function order_types_data($a, $b) {
+    if ($a["order"] == 0) {
+	return true;
+    }
+    if ($b["order"] == 0) {
+	return false;
+    }
+    return $a["order"] > $b["order"];
+}
+
+/**
+ * @param $params
+ * @param $content
+ * @param Smarty $smarty
+ * @param Boolean $repeat
+ *
+ * require function getTypeLink
+ */
+function smarty_block_zq_typelist($params, $content, $smarty, &$repeat, $template) {
+    if (isset($content)) {
+	$db = zq_core::load_model('type_model');
+	$data = $db->select();
+	usort($data, 'order_types_data');
+
+	$_template = $content;
+	$content = "";
+	for ($c = 0; $c < count($data); ++$c) {
+	    $modelInfo = $data[$c];
+	    
+	    if ($modelInfo["ishidden"]) {
+		continue;
+	    }
+
+	    $currentClass = "";
+	    if ($modelInfo["name"] == ROUTE_M && isset($params["currentClass"])) {
+	        $currentClass = $params["currentClass"];
+	    }
+
+	    //$typelink = getTypeLink();
+	    $typelink = "";
+	    $str = str_replace('~name~', $modelInfo['name'], $_template);
+	    $str = str_replace('~link~', $typelink, $str);
+	    $str = str_replace('~title~', $modelInfo['title'], $str);
+	    $str = str_replace('~class~', $currentClass, $str);
+
+	    $content .= $str;
+	}
+
+	return $content;
+    }
+}
+
+?>
--- zqcms/libs/plugins/function.get_today_kaifu_count.php
+++ zqcms/libs/plugins/function.get_today_kaifu_count.php
@@ -9,7 +9,17 @@
 
 
 function smarty_function_get_today_kaifu_count($params, $template) {
-    zq::load_model("kaifu_model");
+    zq_core::load_model("kaifu_model");
     //得到今日的开服数量
+  //   dede:type typeid=6}<a class="white" href="[field:typelink function=GetRealTypeUrl(@me)/]" target="_blank">dede:type dede:php}
+		//     global $dsql;
+		//     $time = strtotime('today');
+		//     $begin_date = mktime(0, 0, 0, date("m", $time), date("d", $time), date("Y", $time));
+		//     $end_date   = mktime(0, 0, 0, date("m", $time), date("d", $time) + 1, date("Y", $time));
+		//     $sql = "SELECT count(*) AS dd FROM `#@__addonkaifu` WHERE test_date >= $begin_date AND test_date < $end_date";
+		//     $count = $dsql->GetOne($sql);
+		//     echo $count['dd'];
+		// /dede:php}</a>
+   	return 111;
 }
 ?>
