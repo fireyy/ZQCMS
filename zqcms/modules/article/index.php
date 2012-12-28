21c52a580ef1b43974cb498831297d4daa7bb97a
fireyy <fireyy@gmail.com>
2012-12-28 11:58:48 +0800

    新闻内页模板套用

--- zqcms/modules/article/index.php
+++ zqcms/modules/article/index.php
@@ -2,23 +2,36 @@
 defined("IN_ZQCMS") or exit("Permission deiened");
 
 class index {
-    function __construct() {
+    public function __construct() {
 	$this->db = zq_core::load_model('article_model');
     }
 
     //内容页
     public function show() {
 	$id = intval($_GET['id']);
-
-	//register_template_data('article', $article);
-
-	//template('article', 'content');
+  if(isset($id) && !empty($id)){
+    $article = $this->db->get_one(array(
+      "id" => $id
+    ));
+  	register_template_data('article', $article);
+    if(!empty($article['game_id'])){
+      $game_db = zq_core::load_model('game_model');
+      $game = $game_db->get_one(array(
+        "id" => $article['game_id']
+      ));
+      $game["url"] = getURL($game);
+      register_template_data('game', $game);
+    }
+  	return template('article', 'content');
+  }else{
+    ShowMsg("未找到对应的文章！","-1");
+  }
     }
 
     //列表页面
-    public function list() {
-	$page = $_GET['page'];
-	//template('article', 'content');
+    public function lists() {
+	#$page = $_GET['page'];
+	return template('article', 'list');
     }
 }
 ?>
