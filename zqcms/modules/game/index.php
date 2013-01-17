c91f12336ecd0ad291b80a02e597b22b57b1e5c8
wolftankk <wolftankk@gmail.com>
2013-01-17 23:00:52 +0800

    重新游戏内页开服信息表, 改进寻找算法. 现在可以正常显示了

--- zqcms/modules/game/index.php
+++ zqcms/modules/game/index.php
@@ -2,115 +2,126 @@
 defined("IN_ZQCMS") or exit("Permission deiened");
 
 class index {
+    private $MAX_KAIFUS = 10;
     public function __construct() {
 	$this->db = zq_core::load_model('game_model');
+    }
+
+    private function getKaifuInfo($timesql, $orderway='ASC') {
+	$now = mktime(0, 0, 0);
+	$last_now = mktime(23, 59, 59);
+
+	$data = $kaifu_db->select(
+	    "game_id = {$game['game_id']} AND $timesql",
+	    '*',
+	    '0, 10',
+	    "test_date {$orderway}"
+	);
+
+	for ($i = 0; $i < count($data); $i++) {
+	    $company_id = $data[$i]['oper_id'];
+	    if (empty($company_id)) {
+		continue;
+	    }
+
+	    if (empty($this->kaifus[$company_id])) {
+		$company = $company_db->get_one(array('company_id'=>$company_id));
+		if (!empty($company)) {
+		    $company['url'] = getURL($company);
+
+		    $this->kaifus[$company_id] = array(
+			'company' => $company,
+			'oper_short_name' => $data[$i]['oper_short_name'],
+			'url' => $data[$i]['register_url'],
+			'kaifu' => array(
+			    'y' => array(),
+			    'n' => array(),
+			    't' => array()
+			)   
+		    );
+		}
+	    }
+
+	    if (!empty($this->kaifus[$company_id])) {
+		$currentTestDate = $data[$i]["test_date"];
+
+		//开始插入数据
+		if ($currentTestDate >= $now && $currentTestDate <= $last_now) {
+		    $this->kaifus[$company_id]['kaifu']['n'][] = $data[$i];
+		}
+
+		//最近的一条
+		if (empty($this->kaifus[$company_id]['kaifu']['y'])) {
+		    if ($currentTestDate < $now) {
+			$this->kaifus[$company_id]['kaifu']['y'][] = $data[$i];
+		    } else {
+			$r = $kaifu_db->get_one(
+			    "oper_id={$company_id} AND game_id={$game['game_id']} AND test_date < {$currentTestDate}", '*', 'test_date DESC'
+			);
+			if (!empty($r)) {
+			    $this->kaifus[$company_id]['kaifu']['y'][] = $r;
+			}
+		    }
+		}
+
+		//后一条
+		if (empty($this->kaifus[$company_id]['kaifu']['t'])) {
+		    if ($currentTestDate > $last_now) {
+			$this->kaifus[$company_id]['kaifu']['t'][] = $data[$i];
+		    } else {
+			$r = $kaifu_db->get_one(
+			    "oper_id={$company_id} AND game_id={$game['game_id']} AND test_date > {$data[$i]['test_date']}", '*', 'test_date ASC'
+			);
+			if (!empty($r)) {
+			    $this->kaifus[$company_id]['kaifu']['t'][] = $r;
+			}
+		    }
+		}
+	    }
+	}
     }
 
     //内容页
     public function show() {
 	$id = intval($_GET['id']);
-	$now = mktime(0, 0, 0);
-	$last_now = mktime(23, 59, 59);
-
 	if(isset($id) && !empty($id)){
 	    $game = $this->db->get_one(array(
 		"id" => $id
 	    ));
-      $maxd = 10;
+	    
+	    if (empty($game)) {
+		ShowMsg("未找到对应的游戏！","-1");
+		return;
+	    }
+
 	    //获得游戏的运营商数量
 	    $rdb = zq_core::load_model('game_company_model');
 	    $company_count = $rdb->count(array('game_id' => $game['game_id']));
 	    $game['company_count'] = $company_count;
 
+	    $this->kaifus = {}
+
 	    //获取开服信息
-	    $kaifu_db = zq_core::load_model('kaifu_model');
-	    $company_db = zq_core::load_model('company_model');
-
-	    $data = $kaifu_db->select(
-		"game_id = {$game['game_id']} AND test_date >= {$now} AND test_date <= {$last_now}",
-		'*',
-		'0, 10',
-		'test_date ASC'
-	    );
-      $countd = count($data);
-      if($countd < $maxd){
-        $tmp = $maxd - $countd;
-  	    $data2 = $kaifu_db->select(
-  		"game_id = {$game['game_id']} AND test_date > {$last_now}",
-  		'*',
-  		"0, {$tmp}",
-  		'test_date ASC'
-  	    );
-        $data = array_merge($data, $data2);
-        $countd = count($data);
-        if($countd < $maxd){
-          $tmp = $maxd - $countd;
-    	    $data2 = $kaifu_db->select(
-    		"game_id = {$game['game_id']} AND test_date < {$now}",
-    		'*',
-    		"0, {$tmp}",
-    		'test_date ASC'
-    	    );
-          $data = array_merge($data, $data2);
-        }
-      }
-      //print_r($data);
+	    $this->kaifu_db = zq_core::load_model('kaifu_model');
+	    $this->company_db = zq_core::load_model('company_model');
 	    
-	    $kaifus = array();
-	    for ($i = 0; $i < count($data); $i++) {
-		$company_id = $data[$i]['oper_id'];
-		if ($company_id) {
-		    if (empty($kaifus[$company_id])) {
-			$company = $company_db->get_one(array('company_id'=>$company_id));
 
-			if (!empty($company)) {
-			    $company['url'] = getURL($company);
+	    $now = mktime(0, 0, 0);
+	    $last_now = mktime(23, 59, 59);
+	    $this->getKaifuInfo("test_date >= {$now} AND test_date <= {$last_now}");
 
-			    $kaifus[$company_id] = array(
-				'company' => $company,
-				'oper_short_name' => $data[$i]['oper_short_name'],
-				'url' => $data[$i]['register_url'],
-				'kaifu' => array(
-				    'y' => array(),
-				    'n' => array(),
-				    't' => array()
-				)   
-			    );
-			}
-		    }
-
-		    if (!empty($kaifus[$company_id])) {
-			//开始插入数据
-			$kaifus[$company_id]['kaifu']['n'][] = $data[$i];
-
-			//最近的一条
-			if (empty($kaifus[$company_id]['kaifu']['y'])) {
-			    $r = $kaifu_db->get_one(
-				"oper_id={$company_id} AND game_id={$game['game_id']} AND test_date < {$data[$i]['test_date']}", '*', 'test_date DESC'
-			    );
-			    if (!empty($r)) {
-				$kaifus[$company_id]['kaifu']['y'][] = $r;
-			    }
-			}
-
-			//后一条
-			if (empty($kaifus[$company_id]['kaifu']['t'])) {
-			    $r = $kaifu_db->get_one(
-				"oper_id={$company_id} AND game_id={$game['game_id']} AND test_date > {$data[$i]['test_date']}", '*', 'test_date ASC'
-			    );
-			    if (!empty($r)) {
-				$kaifus[$company_id]['kaifu']['t'][] = $r;
-			    }
-			}
-		    }
-		}
+	    if (count(array_keys($this->kaifus)) < $this->MAX_KAIFUS) {
+		$this->getKaifuInfo("test_date < {$now}", "DESC");
 	    }
 
-	    register_template_data('kaifus', $kaifus);
+	    if (count(array_keys($this->kaifus)) < $this->MAX_KAIFUS) {
+		$this->getKaifuInfo("test_date > {$last_now}", "ASC");
+	    }
 
+	    register_template_data('kaifus', $this->kaifus);
 	    register_template_data('game', $game);
 	    return template('game', 'content');
-	}else{
+	} else {
 	    ShowMsg("未找到对应的游戏！","-1");
 	}
     }
@@ -118,7 +129,7 @@ class index {
     //列表页面
     public function lists() {
 	$page = isset($_GET['page']) ? $_GET['page'] : 1;
-  $tag_name = isset($_GET['tag']) ? $_GET['tag'] : '';
+	$tag_name = isset($_GET['tag']) ? $_GET['tag'] : '';
 	$filters = array("game_tag", "game_theme", "game_status", "game_effect");
 	$gamesort = isset($_GET['gamesort']) ? $_GET['gamesort'] : 1;
 	$title = "";
@@ -132,12 +143,12 @@ class index {
 		$title = $tag."_".$title;
 	    }
 	}
-  if(!empty($tag_name)){
-    $ids = getIdsByTagname($tag_name,"*",$this->db->typeid);
-    $ids = join(",", $ids);
-    $where[] = "id in ($ids)";
-    $title = $tag_name."_".$title;
-  }
+	if(!empty($tag_name)){
+	    $ids = getIdsByTagname($tag_name,"*",$this->db->typeid);
+	    $ids = join(",", $ids);
+	    $where[] = "id in ($ids)";
+	    $title = $tag_name."_".$title;
+	}
 	switch ($gamesort) {
 	case 1:
 	    $orderby = "pubdate DESC";
