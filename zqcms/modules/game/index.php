f6f477d1b5bd0a3aafd7e37af684c0e99dc356e9
fireyy <fireyy@gmail.com>
2013-01-30 16:45:44 +0800

    解决游戏标签的bug

--- zqcms/modules/game/index.php
+++ zqcms/modules/game/index.php
@@ -150,7 +150,8 @@ class index {
 	$title = getTypeName($this->db->typeid);
 	foreach ($filters as $key => $value) {
 	    $tag = $value[$_GET[$key]];
-	    if(isEmpty($tag)){
+	    echo $tag;
+	    if(isEmpty($tag) && $tag!="全部"){
 		$where[] = "$key = '".$tag."'";
 		$title = $tag."_".$title;
 	    }
